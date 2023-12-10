87b159d5fd9104cb2018c316aca5546eaf0407c7
Aman Sirohi <amansirohi077@gmail.com>
2023-12-10 17:11:32 +0530

    added login logout button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,30 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 
 const Navbar = () => {
-    return (
-        <div className="navbar-cont">
-            <div className="logo-cont">
-                <img
-                    src={LOGO_URL}
-                    alt=""
-                    className="logo"
-                />
-            </div>
-            <div className="nav-items">
-                <ul>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Contact Us</li>
-                    <li>Cart</li>
-                </ul>
-            </div>
-        </div>
-    );
+  const [login, setLogin] = useState("Login");
+  return (
+    <div className="navbar-cont">
+      <div className="logo-cont">
+        <img src={LOGO_URL} alt="" className="logo" />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              login === "Login" ? setLogin("Logout") : setLogin("Login");
+            }}
+          >
+            {login}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
 };
 export default Navbar;
