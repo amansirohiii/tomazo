7cce079996ae84b9a0f8debe136539ec06331973
Aman Sirohi <amansirohi077@gmail.com>
2024-01-13 16:02:50 +0530

    prod api url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,5 +2,5 @@ export const CDN_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_los
 export const LOGO_URL="https://img.freepik.com/premium-vector/kitchen-chef-logo-design-vector-template_15146-1164.jpg?size=626&ext=jpg&ga=GA1.1.1729305986.1696401089&semt=ais";
 // export const RES_API="https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.5793735&lng=77.45011819999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
 // export const MENU_API="https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.5793735&lng=77.45011819999999&restaurantId="
-export const RES_API="http://localhost:5000/api/restaurants?lat=28.5793735&lng=77.45011819999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-export const MENU_API="http://localhost:5000/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=28.5793735&lng=77.45011819999999&restaurantId="
+export const RES_API="https://gpt-netflix-backend.vercel.app/api/restaurants?lat=28.5793735&lng=77.45011819999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+export const MENU_API="https://gpt-netflix-backend.vercel.app/api/restaurants/menu?page-type=REGULAR_MENU&complete-menu=true&lat=28.5793735&lng=77.45011819999999&restaurantId="
